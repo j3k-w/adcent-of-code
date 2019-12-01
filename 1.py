fd08cb1fd898f254dd0cba9cd7994fe86b4398a7
j3k-w <j3k.walczak@gmail.com>
2019-12-01 15:46:39 +0100

    first day

---
+++ 1.py
@@ -0,0 +1,30 @@
+def calculate():
+    sum = 0
+    with open('input.txt') as inp:
+        for mass in inp:
+            sum += (int(mass) // 3) - 2
+    return sum
+
+def fuel_r(mass):
+    single_sum = 0
+    r_fuel = mass  # required fuel
+
+    while True:
+        r_fuel = (r_fuel // 3) - 2
+        if r_fuel > 0:
+            single_sum += r_fuel
+        else:
+            break
+
+    return single_sum
+
+def calculate_recursively():
+    sum = 0
+    with open('input.txt') as inp:
+        for mass in inp:
+            sum += fuel_r(int(mass))
+    return sum
+
+
+print(calculate())
+print(calculate_recursively())
