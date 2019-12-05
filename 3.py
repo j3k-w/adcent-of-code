73e3c03ede99dc8c9eb50261ac2c903f21689156
j3k <j3k.walczak@gmail.com>
2019-12-05 15:04:32 +0100

    added 4th day

--- 3.py
+++ 3.py
@@ -39,7 +39,6 @@ def compare(s1: list, s2: list) -> list:
                   el_i[1] > el_j[1]) and
                  (ne_i[0] > ne_j[0] and
                   ne_i[1] < ne_j[1]))):
-                # meeting_points.append([[el_i, el_j], [ne_i, ne_j]])
                 meeting_points.append([[el_i, ne_i], [el_j, ne_j]])
 
     return meeting_points
@@ -47,36 +46,35 @@ def compare(s1: list, s2: list) -> list:
 
 def parse_vectors(mpp):
     "Parses the vectors of crossing points"
-    x, y = 0, 0
-    # true_meets = []
+    true_meets = []
 
     for change in mpp:
         for vector in change:
-            print(max(set(vector), key=vector.count))
+            if vector[0][1] == vector[1][1]:
+                x = vector[0][1]
+            elif vector[0][0] == vector[1][0]:
+                x = vector[0][0]
+            true_meets.append(abs(x))
 
-            # if vector[0][1] == vector[1][1]:
-        #         x = vector[0][1]
-        #         y = vector[1][0]
-        #     elif vector[0][0] == vector[1][0]:
-        #         x = vector[0][0]
-        #         y = vector[0][1]
-        # print([x, y])
+    print(true_meets)
+    true_meets = [true_meets[i:i+2] for i in range(0, len(true_meets), 2)]
 
-    # for i, m in enumerate(mpp):
-    #     if m[i][0][1] == m[i][0][1]:
-    #         y = m[i][0][1]
-    #         x = m[i][1][0]
-    #     elif m[i][0][0] == m[i][1][0]:
-    #         y = m[i][1][0]
-    #         x = m[i][0][1]
+    print(true_meets)
 
-    #     true_meets.append([x, y])
+    result = sum(min(true_meets, key=sum))
+    print(result)
 
-    # return true_meets
+    return result
 
 
-wire1 = ['R8', 'U5', 'L5', 'D3']
-wire2 = ['U7', 'R6', 'D4', 'L4']
+# wire1 = ['R8', 'U5', 'L5', 'D3']
+# wire2 = ['U7', 'R6', 'D4', 'L4']
+
+# wire1 = ['R75','D30','R83','U83','L12','D49','R71','U7','L72']
+# wire2 = ['U62','R66','U55','R34','D71','R55','D58','R38']
+
+wire1 = ['R98','U47','R26','D63','R33','U87','L62','D20','R33','U53','R51']
+wire2 = ['U98','R91','D20','R16','D67','R40','U7','R15','U6','R7']
 
 s1 = compute(wire1)
 s2 = compute(wire2)
