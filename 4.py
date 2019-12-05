b16942eeee3bf1e8f9da3c9d81ddee975f27b456
Jacek Walczak <42350899+j3k-w@users.noreply.github.com>
2019-12-05 15:05:57 +0100

    4th day is done

--- 4.py
+++ 4.py
@@ -1,26 +1,50 @@
+import math
+
+
+def repetition(number: str) -> bool:
+    result = False
+    for i in range(len(number) - 1):
+        if number[i] == number[i+1]:
+            result = True
+            return result
+
+    return result
+
+
+def repetition2(num: str) -> bool:
+    new_num = []
+    for i in range(math.floor(len(num)/2)):
+        new_num.append([num[i*2], num[i*2+1]])
+
+    for i in new_num:
+        if i[0] != i[1]:
+            return False
+
+    return True
+
+
+def non_decreasing(number: str) -> bool:
+    result = True
+    for i in range(len(number) - 1):
+        if number[i] > number[i + 1]:
+            result = False
+            break
+
+    return result
+
+
 def find(start: int, stop: int) -> int:
     cnt = 0
 
     for num in range(start, stop + 1):
-        if len(str(num)) == 6:
-            # make a list out of number and
-            # delete repeating values
-            num_ = list(dict.fromkeys(str(num)))
-
-            # if there is a repeating value
-            if len(num_) < 6:
-                print(num_)
-                for i, s in enumerate(num_):
-                    if s <= num_[i+1]:
-                        print(num_)
-            # if str(num) in '1234567890'
-            # num_ = list(dict.fromkeys(num))
-
-        # cnt += 1
+        num = str(num)
+        if non_decreasing(num):
+            if repetition(num) or repetition2(num):
+                cnt += 1
     return cnt
 
 
-start = 111111
-stop  = 111114
-# print(find(start, stop))
-find(start, stop)
+start = 264793
+stop = 803935
+# for i in range(start, stop)
+print(find(start, stop))
