b3ca0ddd7318a0c37ab602ca69bf50b9ce467e75
j3k-w <j3k.walczak@gmail.com>
2019-12-08 23:29:04 +0100

    im really close to the solution

--- 5.py
+++ 5.py
@@ -1,4 +1,7 @@
-def compute(src: list) -> int:
+def compute(src: list):
+    ID = input("Provide an ID of the system to test: ")
+    if int(ID) != 1:
+        return "-- wrong ID -- system halted!"
     inp = src[:]
     pos = 0
 
@@ -15,28 +18,28 @@ def compute(src: list) -> int:
                 return inp
             elif opcode in (1, 2):
                 # FIRST PARAMETER; FAC1
-                if instr[2]:
+                if len(instr) >= 2 and instr[2] == "1":
                     fac1 = command[1]
-                    print(f"fac1 mode: {instr[1]}, fac1 value: {fac1}")
+                    print(f"fac1 mode: {instr[2]}, fac1 value: {fac1}")
                 else:
                     fac1 = inp[command[1]]
-                    print(f"fac1 mode: {instr[1]}, fac1 value: {fac1}")
+                    print(f"fac1 mode: {instr[2]}, fac1 value: {fac1}")
 
                 # SECOND PARAMETER; FAC2
-                if instr[3]:
+                if len(instr) >= 4 and instr[3] == "1":
                     fac2 = command[2]
-                    print(f"fac2 mode: {instr[2]}, fac2 value: {fac2}")
+                    print(f"fac2 mode: {instr[3]}, fac2 value: {fac2}")
                 else:
                     fac2 = inp[command[2]]
-                    print(f"fac2 mode: {instr[2]}, fac2 value: {fac2}")
+                    print(f"fac2 mode: {instr[3]}, fac2 value: {fac2}")
 
                 # THIRD PARAMETER; TARGET
-                if instr[4]:
+                if len(instr) == 5 and instr[4] == "1":
                     target = inp[command[3]]
-                    print(f"target mode: {instr[3]}, target value: {target}")
+                    print(f"target mode: {instr[4]}, target value: {target}")
                 else:
                     target = command[3]
-                    print(f"target mode: {instr[3]}, target value: {target}")
+                    print(f"target mode: 0, target value: {target}")
 
                 if opcode == 1:
                     inp[target] = fac1 + fac2
@@ -44,7 +47,17 @@ def compute(src: list) -> int:
                 elif opcode == 2:
                     inp[target] = fac1 * fac2
                     print(f"element {target} is now {inp[target]}")
-            pos += 4
+                pos += 4
+
+            elif opcode in (3, 4):
+                print(opcode)
+                if opcode == 3:
+                    inn = inp[inp[pos+1]]
+                    print("opcode 3; value:", inn)
+                elif opcode == 4:
+                    print(inp[pos+1])
+
+                pos += 2
 
     return inp
 
@@ -53,26 +66,7 @@ if __name__=="__main__":
     # with open('5input.txt') as f:
     #     src = [int(x) for x in next(f).split(',')]
     src = [1002, 4, 3, 4, 33]
-    print("before:", src)
-    print("after:", compute(src))
-
-
-
-def c2():
-    for index in range(0, len(inp), 4):
-        opcode = inp[index]
-        fac1 = inp[inp[index + 1]]
-        fac2 = inp[inp[index + 2]]
-        target = inp[index + 3]
-
-        if opcode == 99:
-            return inp[0]
-        elif opcode == 1:
-            inp[target] = fac1 + fac2
-        elif opcode == 2:
-            inp[target] = fac1 * fac2
-        elif opcode == 3:
-            inp[target] = fac1 * fac2
-
-
-    return inp[0]
+    srv = [1101, 100, -1, 4, 0]
+    srb = [3, 0, 4, 0, 99]
+    print("before:", srb)
+    print("after:", compute(srb))
