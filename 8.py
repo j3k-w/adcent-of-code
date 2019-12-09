e5aa391bd90fe10770786eff7c78488aea232a60
j3k-w <j3k.walczak@gmail.com>
2019-12-09 12:43:46 +0100

    savepoint; working on the second part is going well

--- 8.py
+++ 8.py
@@ -1,24 +1,42 @@
-def decode(f: str):
-    off = 25 * 6
-
-    layers = [f[i:i+off] for i in range(0, len(f), off)]
-
-    minn = off
-    minn_idx = 0
+def check_transmission(layers: list) -> int:
+    minn = 25 * 6
+    m_idx = 0
 
     for idx, layer in enumerate(layers):
         if layer.count('0') < minn:
-            print(f"found less zeros at {idx} layer")
             minn = layer.count('0')
-            minn_idx = idx
+            m_idx = idx
 
-    result = layers[minn_idx].count('1') * layers[minn_idx].count('2')
+    return layers[m_idx].count('1') * layers[m_idx].count('2')
 
-    return result
 
+def decode(layers: list):
+    # 0 -> black
+    # 1 -> white
+    # 2 -> transparent
+    final_layer = ''
+
+    for layer in layers:
+        for i in layer:
+            if i == '0' or i == '1':
+                final_layer += i
+            else:
+                break
+
+    print(len(final_layer))
+
+    return final_layer
 
 if __name__=="__main__":
     with open('8input.txt') as f:
-        f = f.readline().strip()
 
-        print(decode(str(f)))
+        # Preparing data.
+        f = f.readline().strip()
+        off = 25 * 6
+        layers = [f[i:i+off] for i in range(0, len(f), off)]
+
+        # First part.
+        print(check_transmission(layers))
+
+        # Second part.
+        print(decode(layers))
